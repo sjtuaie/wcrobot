a017796a2764c9b3e4795bbb0a766b08477580ab
sjtuaie <sjtuaie@163.com>
2017-10-14 23:00:30 +0800

    first commit

---
+++ wcrobot/urls.py
@@ -0,0 +1,38 @@
+"""wcrobot URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from werobot.contrib.django import make_view
+from wcrobot.robot import robot
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+urlpatterns = [
+    url(r'^robot/', make_view(robot)),
+    url(r'^admin/', admin.site.urls),
+] + static(MEDIA_URL, document_root=MEDIA_ROOT) + staticfiles_urlpatterns()
+
+
